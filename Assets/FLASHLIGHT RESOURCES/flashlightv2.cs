6a0805337f70ce1dbf99bdc014d65682d575ecc0
MAXOSAUCE <maxosauce@proton.me>
2024-05-21 21:34:47 +0800

    flashlights, tablelamps
    working with script

---
+++ Assets/FLASHLIGHT RESOURCES/flashlightv2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlightv2 : MonoBehaviour
+
+{
+    public Material lens;
+    public Light spotlight; // Reference to the spotlight
+    public Light pointlight; // Reference to the point light
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void LightToggle()
+    {
+        _audioSource.Play();
+        bool lightState = !spotlight.enabled; // Use the spotlight's current state to determine the new state
+
+        // Toggle both lights
+        spotlight.enabled = lightState;
+        pointlight.enabled = lightState;
+    }
+}
+
